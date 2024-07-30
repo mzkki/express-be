c5900bc78b6e0d4f4715af966b520aad57ed28c7
mzkki <08harismu@gmail.com>
2024-07-30 13:07:57 +0800

    feat(login): finish login controller

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const registerController = require('../controllers/RegisterController');
+const loginController = require('../controllers/LoginController');
 
-const { validateRegister } = require('../utils/validators/auth');
+const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
 router.post('/register', validateRegister, registerController.register);
+router.post('/login', validateLogin, loginController.login);
 
 module.exports = router;
